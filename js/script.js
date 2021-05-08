510feaef4edc5c16dad200737bdda5f50bb288e3
Cat <morancathy@gmail.com>
2021-05-08 14:39:46 -0500

    domlab update part1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,39 @@
+// Task 3.0     Menu data structure
+var menuLinks = [
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '/catalog'},
+  {text: 'orders', href: '/orders'},
+  {text: 'account', href: '/account'},
+];
+
+//Task 1.0
 const mainEl = document.querySelector('main');
-const backgroundColor = 'var(--main-bg)';
-mainEl = <h1>SEI Rocks!</h1>;
-Element.classList API
+//Task 1.1
+mainEl.style.backgroundColor = 'var(--main-bg)';
+
+//Task 1.2
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
+
+//Task 1.3
+mainEl.classList.add('flex-ctr');
+
+
+//Task 2.0
+const topMenuEl = document.getElementById('top-menu');
+//Task 2.1
+topMenuEl.style.height = '100%';
+//Task 2.2
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+//Task 2.3
+topMenuEl.classList.add('flex-around');
+
+//3.1
+for(let link of menuLinks) {
+  const linkTag = document.createElement('a');
+  linkTag.setAttribute('href', link.href);
+  linkTag.innerHTML = link.text;
+  topMenuEl.append(linkTag);
+  console.log(linkTag)
+  // console.log(link)
+
+};
