0c293f129a6d38cc9b18545617fc6f34e8504345
Cat <morancathy@gmail.com>
2021-05-12 13:52:57 -0500

    Update Homework w3d3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// Task 3.0     Menu data structure
 var menuLinks = [
   {text: 'about', href: '/about'},
   {text: 'catalog', href: '#', subLinks: [
@@ -16,80 +15,113 @@ var menuLinks = [
     {text: 'sign out', href: '/account/signout'},
   ]},
 ];
-
 //Task 1.0
+// Select and cache the <main>element in a variable named mainEl.
 const mainEl = document.querySelector('main');
-//Task 1.1
+
+// Task 1.1
+// Set the background color of mainElto the value stored in the --main-bgCSS custom property.
 mainEl.style.backgroundColor = 'var(--main-bg)';
 
-//Task 1.2
-mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
+// Task 1.2
+// Set the content of mainElto <h1>SEI Rocks!</h1>.
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>'  //innerHTML allows you to set the h1 element without printing it
 
-//Task 1.3
+// Task 1.3
+// Add a class of flex-ctrto mainEl.
 mainEl.classList.add('flex-ctr');
 
+// Task 2.0
+// Select and cache the <nav id="top-menu">element in a variable named topMenuEl.
+const topMenuEl = document.querySelector('#top-menu');
 
-//Task 2.0
-const topMenuEl = document.getElementById('top-menu');
-//Task 2.1
+// Task 2.1
+// Set the height topMenuElelement to be 100%.
 topMenuEl.style.height = '100%';
-//Task 2.2
+
+// Task 2.2
+// Set the background color of topMenuElto the value stored in the --top-menu-bgCSS custom property.
 topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
-//Task 2.3
+
+// Task 2.3
+// Add a class of flex-aroundto topMenuEl.
 topMenuEl.classList.add('flex-around');
 
-//3.1
-for(let link of menuLinks) {
-  const linkTag = document.createElement('a');
-  linkTag.setAttribute('href', link.href);
-  linkTag.innerHTML = link.text;
-  topMenuEl.append(linkTag);
+//Task 3.1
+// Iterate over the entire menuLinksarray and for each "link" object:
+// Create an <a>element.
+// On the new element, add an hrefattribute with its value set to the hrefproperty of the "link" object.
+// Set the new element's content to the value of the textproperty of the "link" object.
+// Append the new element to the topMenuElelement.
+for(let link of menuLinks){
+  const aEl = document.createElement('a');
+  aEl.setAttribute("href", link.href);
+  aEl.innerHTML = link.text;
+  topMenuEl.appendChild(aEl);   //need more clarification
 }
-//4.0
-const subMenuEl = document.getElementById('sub-menu');
 
-//4.1
-subMenuEl.style.height = '100%';
+// Task 4.0
+// Select and cache the <nav id="sub-menu">element in a variable named subMenuEl.
+const subMenuEl = document.querySelector('#sub-menu');
 
-//4.2
+// Task 4.1
+// Set the height subMenuElelement to be 100%.
+subMenuEl.style.height = "100%";
+
+// Task 4.2
+// Set the background color of subMenuElto the value stored in the --sub-menu-bgCSS custom property.
 subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)';
 
-//4.3
+// Task 4.3
+// Add the class of flex-aroundto the subMenuElelement.
 subMenuEl.classList.add('flex-around');
-console.log(subMenuEl)
-//4.4
+
+// Task 4.4
+// Set the CSS positionproperty of subMenuElto the value of absolute.
 subMenuEl.style.position = 'absolute';
+// Task 4.5
+// Set the CSS topproperty of subMenuElto the value of 0.
+subMenuEl.style.top = '0px';
 
-//4.5
-subMenuEl.style.top = '0';
-
-//5.1
+// Task 5.1
+// Select and cache the all of the <a>elements inside of topMenuElin a variable named topMenuLinks.
+// Declare a global showingSubMenuvariable and initialize it to false;
 const topMenuLinks = document.querySelectorAll('#top-menu a');
-let showingSubMenu = false;
+var showingSubMenu = false;
 
-//5.2
-topMenuEl.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  const link = evt.target //target is what fired the event
-  // console.log(link);
-  if (link.tagName != 'A'){
-    console.log(link.tagName);
+// Task 5.2
+// Attach a delegated 'click' event listener to topMenuEl.
+// The first line of code of the event listener function should call the event object's preventDefault()method.
+// The second line of code function should immediately return if the element clicked was not an <a>element.
+// console.logthe content of the <a>to verify the handler is working.
+
+topMenuEl.addEventListener('click', (myFunction) => {  //why this arrow
+  myFunction.preventDefault();    //what is preventDefault
+  const pressed = myFunction.target;  //target is what fired the event
+  if(pressed.tagName != 'A'){
     return;
-  } else {
-    console.log(link.textContent);
+  }else{
+    pressed.classList.add('active');
+    console.log(pressed.text)  //difference b/w text and textContent?
   }
-// 5.3
-  const checker = link.classList.contains('active');
-  console.log(checker)
-  if(checker === 'active'){
-      link.classList.remove('active');
-      showingSubMenu = false;
-      subMenuEl.style.top = '0';
-      return;
+  if(pressed.classList.contains('active')){
+    console.log(pressed.classList.contains('active'));
+    pressed.classList.remove('active');
+    showingSubMenu = false;
+    subMenuEl.style.top = '0px';
+    return;
   }
-  //5.5
-  if(!checker){
-    link.classList.add('active');
-  }
-});
-// 5.4
+}); //this consoles the text of the clicked text. Nothing else if clicked elswhere
+
+// Task 5.3
+// Next in the event listener, if the clicked <a>link has a class of active:
+// Remove the activeclass from the clicked <a>element.
+// Set the showingSubMenuto false.
+// Set the CSS topproperty of subMenuElto 0.
+// returnto exit the handler.
+// Task 5.4
+// Next, the event listener should remove a class name of activefrom each <a>element in topMenuLinks- whether the activeclass exists or not.
+// Task 5.5
+// Next, the event listener should add a class name of activeto the <a>element that was clicked.
+// Task 5.6
+// Set showingSubMenuto trueif the clicked <a>element's "link" object within menuLinkshas a subLinksproperty (all do, except for the "link" object for ABOUT), otherwise, set it to false.
